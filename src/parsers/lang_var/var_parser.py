e0fd5f187ca1cf2e74c3601c419d377816b4e871
dikanstudio <allfromleon@gmail.com>
2024-05-08 10:47:19 +0200

    LAB-03-first attempts

---
+++ src/parsers/lang_var/var_parser.py
@@ -0,0 +1,80 @@
+from lark import ParseTree
+from parsers.lang_simple.simple_ast import *
+from parsers.common import *
+import common.log as log
+# import mod
+from lang_var.var_ast import *
+
+grammarFile = "./src/parsers/lang_simple/simple_grammar.lark"
+grammarFile2 = "./src/parsers/lang_var/var_grammar.lark"
+
+def parse(args: ParserArgs) -> exp:
+    parseTree = parseAsTree(args, grammarFile, 'exp')
+    ast = parseTreeToExpAst(parseTree)
+    log.debug(f'AST: {ast}')
+    return ast
+
+def parseTreeToExpAst(t: ParseTree) -> exp:
+    match t.data:
+        # case 'int_exp' to return IntConst
+        case 'int_exp':
+            return IntConst(int(asToken(t.children[0])))
+        # case 'add_exp' to return BinOp with Add
+        case 'add_exp':
+            e1, e2 = [asTree(c) for c in t.children]
+            return BinOp(parseTreeToExpAst(e1), Add(), parseTreeToExpAst(e2))
+        # case 'sub_exp' to return BinOp with Sub
+        case 'sub_exp':
+            e1, e2 = [asTree(c) for c in t.children]
+            return BinOp(parseTreeToExpAst(e1), Sub(), parseTreeToExpAst(e2))
+        # case 'mul_exp' to return BinOp with Mul
+        case 'mul_exp':
+            e1, e2 = [asTree(c) for c in t.children]
+            return BinOp(parseTreeToExpAst(e1), Mul(), parseTreeToExpAst(e2)) 
+        case 'var_exp':
+            return Name(Ident(asToken(t.children[0])))
+        case 'func_exp':
+            # decide if it's print or input_int
+            func = asToken(t.children[0])
+            if func == 'print':
+                # create a call to print
+                return Call(Ident('print'), [parseTreeToExpAst(asTree(t.children[2]))])
+            elif func == 'input_int':
+                # create a call to input_int
+                return Call(Ident('input_int'), [])
+            else:
+                raise Exception(f'unhandled function {func}')
+        case 'paren_exp':
+            return parseTreeToExpAst(asTree(t.children[1]))
+        case 'line_exp':
+            return parseTreeToExpAst(asTree(t.children[0])) 
+        case 'exp' | 'exp_1' | 'exp_2':
+            return parseTreeToExpAst(asTree(t.children[0]))
+        case _:
+            raise Exception(f'unhandled parse tree of kind {kind} for exp: {t}')
+        
+def parseTreeToStmtAst(t: ParseTree) -> stmt:
+    match t.data:
+        # match two possible cases
+        # like in the var file described
+        case 'exp_stmt':
+            return StmtExp(parseTreeToExpAst(asTree(t.children[0])))
+        case 'assign_stmt':
+            var, exp = [asTree(c) for c in t.children]
+            return Assign(Ident(asToken(var)), parseTreeToExpAst(asTree(exp)))
+        case kind:
+            raise Exception(f'unhandled parse tree of kind {kind} for stmt: {t}')
+        
+def parseTreeToStmtListAst(t: ParseTree) -> list[stmt]:
+    return [parseTreeToStmtAst(asTree(c)) for c in t.children]
+
+def parseTreeToModuleAst(t: ParseTree) -> mod:
+    return Module(parseTreeToStmtListAst(asTree(t.children[0])))
+        
+def parseModule(args: ParserArgs) -> mod:
+    parseTree = parseAsTree(args, grammarFile2, 'lvar')
+    print("AHHHHHH")
+    print(parseTree.pretty())
+    ast = parseTreeToModuleAst(parseTree)
+    log.debug(f'AST: {ast}')
+    return ast
--- src/parsers/tinyJson/tinyJson_parser.py
+++ src/parsers/tinyJson/tinyJson_parser.py
@@ -18,7 +18,15 @@ def ruleJson(toks: TokenStream) -> Json:
             return int(t.value)
         case 'LBRACE':
             print('LBRACE')
-            return ruleEntryList(toks)
+            dictTemp = ruleEntryList(toks)
+            toks.ensureNext('RBRACE')
+            return dictTemp
+        case 'COLON':
+            toks.next()
+            return ruleJson(toks)
+        case 'RBRACE':
+            toks.next()
+            return ruleJson(toks)
         case _:
             raise ParseError(f'Unexpected token {t}')
 
@@ -27,25 +35,27 @@ def ruleEntryList(toks: TokenStream) -> dict[str, Json]:
     Parses the content of a JSON object.
     """
     # {"k1": 1}
-    t = toks.next()
+    t = toks.lookahead()
     match t.type:
-        case 'RBRACE':
-            print('RBRACE')
-            return {}
         case 'STRING':
             print('STRING')
             key = t.value[1:-1]
+            toks.next()
             toks.ensureNext('COLON')
             value = ruleJson(toks)
-            if toks.lookahead() == "COMMA":
+            a = {key : value}
+            if toks.lookahead().type == "COMMA":
+                toks.next()
+                return {**a, **ruleEntryList(toks)}
+                # TODO
                 # if there are more entryies
-                return {key: value}
+            #    return {key: value}
             # print dict
             print({key: value})
             # return dict containing key-value pair
             return {key: value}
         case _:
-            raise ParseError(f'Unexpected token {t}')
+            return {}
 
 def parse(code: str) -> Json:
     parser = mkLexer("./src/parsers/tinyJson/tinyJson_grammar.lark")
